aef179dad68c47ced2e728eda3f52cf87d806721
Juan M. Chavez Jr <juan@dexteroustechnology.com>
2016-12-08 14:32:19 -0800

    uncomment some log messages
    uncomment some log messages

--- sample/js/index.js
+++ sample/js/index.js
@@ -51,9 +51,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        // app.receivedEvent('deviceready');
+         app.receivedEvent('deviceready');
         app.logArea = document.getElementById("log-area");
-      //  app.log("Cordova initialized, 'deviceready' event was fired");
+        app.log("Cordova initialized, 'deviceready' event was fired");
         AuthenticationContext = Microsoft.ADAL.AuthenticationContext;
         app.createContext();
         
